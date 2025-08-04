37c4c767e06eb80c2bd60ac532605a4c67d90051
Ignacio Rognoni <rognoni.ignacio@gmail.com>
2025-08-04 19:02:43 +0200

    refactor heroes to interfaces and clean up scaffolding

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -14,10 +11,10 @@ module.exports = function (config) {
     ],
     client: {
       jasmine: {},
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true
     },
     coverageReporter: {
       dir: require('path').join(__dirname, '../coverage/angular-tour-of-heroes'),
